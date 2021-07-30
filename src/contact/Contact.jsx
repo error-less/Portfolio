5a0affbd5084965d80dd923d982df6edd606c628
Atul Singh <atul.ks1998@gmail.com>
2021-07-30 11:54:17 +0530

    contact and testemonials added

--- src/contact/Contact.jsx
+++ src/contact/Contact.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
-import './contact.scss'
+import React, { useState } from "react";
+import "./contact.scss";
 function Contact() {
-    return (
-        <div className='contact'>
-            dfgdfg
-        </div>
-    )
+  const [message, setMessage] = useState(false);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setMessage(true);
+  };
+  return (
+    <div className="contact" id="contact">
+      <div className="left">
+        <img src="assets/shake.svg" alt="" />
+      </div>
+      <div className="right">
+        <h2>Contact.</h2>
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="Email" />
+          <textarea placeholder="Message"></textarea>
+          <button type="submit">Send</button>
+          {message && <span>Thanks, I'll reply ASAP !!</span>}
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
--- src/intro/Intro.jsx
+++ src/intro/Intro.jsx
@@ -1,10 +1,40 @@
 import React from 'react'
 import './intro.scss'
+import { init } from "ityped";
+import { useEffect, useRef } from "react";
 function Intro() {
+    const textRef = useRef();
+
+    useEffect(() => {
+      init(textRef.current, {
+        showCursor: true,
+        backDelay: 1500,
+        backSpeed:60,
+        strings: ["Developer", "Designer", "Content Creator"],
+      });
+    }, []);
+
     return (
         <div className="intro">
-            sadfs
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="assets/man.png" alt="" />
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hi There, I'm</h2>
+                    <h1>Atul Singh</h1>
+                    <h3>
+                        Programmer <span ref={textRef}></span>
+                    </h3>
+                </div>
+                <a href="#portfolio">
+                    <img src="assets/down.png" alt="" />
+                </a>
+            </div>
         </div>
+
     )
 }
 
