57c13025242d7320b02efedc89d7de6f92118075
Atul Singh <atul.ks1998@gmail.com>
2021-08-01 23:28:47 +0530

    portfolio added

--- src/portfolio/Portfolio.jsx
+++ src/portfolio/Portfolio.jsx
@@ -8,12 +8,29 @@ function Portfolio() {
              <li>Featured</li>
              <li className='active'>Web App</li>
              <li>Design</li>
-             <li>Branding</li>
+             <li>Machine Learning</li>
+             <li>Datascience</li>
          </ul>
          <div className="container">
              <div className="item">
-                 <img src="" alt="" />
-                 <h3>Banking App</h3>
+                 <img src="assets/duet.png" alt="" />
+                 <h3>Video-Call App</h3>
+             </div>
+             <div className="item">
+                 <img src="assets/netflix.png" alt="" />
+                 <h3>Netflix App</h3>
+             </div>
+             <div className="item">
+                 <img src="assets/portfolio.png" alt="" />
+                 <h3>Portfolio</h3>
+             </div>
+             <div className="item">
+             <img src="assets/todo.png" alt="" />
+                 <h3>Todo App</h3>
+             </div>
+             <div className="item">
+             <img src="assets/drum.png" alt="" />
+                 <h3>Drum-Kit App</h3>
              </div>
          </div>
         </div>
